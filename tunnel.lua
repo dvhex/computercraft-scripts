239af0dd12ef06d8efe78fa08deb5cfe087427e7
vladimir <dabarov@gmail.com>
2014-01-07 19:28:15 +0700

    tunnel через АПИ

--- tunnel.lua
+++ tunnel.lua
@@ -1,44 +1,4 @@
-function rotate()
-    turtle.turnRight()
-    turtle.turnRight()
-end
-
-function putfakel()
-    for i=2,4,1 do
-        if turtle.getItemCount(i) > 1 then
-            turtle.select(i)
-            turtle.placeUp()
-            break
-        end
-    end
-end
-
-function tryRefuel()
-    local result = true;
-    turtle.select(1)
-    if turtle.getItemCount(1) <= 1 then
-        result = false
-    else
-        if turtle.getFuelLevel() < 80 then
-            turtle.refuel(1)
-        end
-    end
-    return result;
-end
-
-function stepForward()
-    tryRefuel()
-    repeat
-        turtle.dig()
-    until turtle.forward()
-end
-
-function tunnel(lenght)
-    for i=0,lenght-1,1 do
-        stepForward()
-        turtle.digDown()
-    end
-end
+os.loadAPI("disk/main")
 
 function selectNoEmpty()
     for i=5,16,1 do
@@ -54,26 +14,21 @@ function back(lenght)
         turtle.digDown()
         selectNoEmpty()
         turtle.placeDown()
-        stepForward()
+        main.stepForward()
         if i % 8 == 0 then
-            putfakel()
+            main.putfakel()
         end
     end
 end
 
-function stepDown()
-    while not turtle.down() do
-        turtle.digDown()
-    end
-end
-
 function work(lenght)
-    tunnel(lenght)
+    main.tunnel(lenght)
     for i=1,2,1 do
-        stepDown()
+        main.stepDown()
     end
-    rotate()
+    main.rotate()
     back(lenght)
 end
 
-work(250)
+print("input size of tunnel: ")
+work(read())
